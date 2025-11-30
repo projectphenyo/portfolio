66855cc682445e9a7bf887ec347a889015179333
Phenyo Moeti <projectphenyo@gmail.com>
2025-11-30 12:45:52 +0200

    added comments

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,2 @@
+// this component defines the main structure of the react app
+//it includes the header and main content
---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,2 @@
+// entry point of my react application
+// it renders the main app components into the root DOM element
