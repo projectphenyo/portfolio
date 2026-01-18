e57cfced5c6a783505c06e52f134148d16e684e6
Phenyo Moeti <projectphenyo@gmail.com>
2026-01-18 16:52:33 +0200

    Updated file structure, Installed Tailwind and planned Sections

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
